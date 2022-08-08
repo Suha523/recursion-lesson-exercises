1ba03a095319a9b3d5e20b09ba7b758227fe8d2b
suha <suhashehahde@gmail.com>
2022-08-08 01:29:53 -0700

    add recursion exercises

--- src/app.js
+++ src/app.js
@@ -4,25 +4,39 @@
 */
 
 //Exercise 1
-const findFactorial = function() {
+const findFactorial = function (number) {
   //Your code here
-
-}
+  if (number === 1) {
+    return 1;
+  } else {
+    return number * findFactorial(number - 1);
+  }
+};
 
 //Exercise 2
-const reverseString = function() {
+const reverseString = function () {
   //Your code here
-
-}
+  if (str.length !== 1) {
+    return reverseString(str.slice(1)) + str.charAt(0);
+  } else {
+    return str;
+  }
+};
 
 //Exercise 3
-const arr1 = [1, 2, 3]
-const arr2 = []
+const arr1 = [1, 2, 3];
+const arr2 = [];
 
-const swap = function() {
+const swap = function (arr1, arr2) {
   //Your code here
 
-}
+  if (arr1.length === 0) {
+    return arr2;
+  } else {
+    arr2.push(arr1[0]);
+    return swap(arr1.slice(1), arr2);
+  }
+};
 
 /* DO NOT REMOVE THE EXPORTS BELOW */
-module.exports = { findFactorial, reverseString, swap }
+module.exports = { findFactorial, reverseString, swap };
